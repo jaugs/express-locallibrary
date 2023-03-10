e88e7f660e4fd8eccf3e7c980eb2e3d9632c28b4
jaugs <johnaugsburger@gmail.com>
2023-03-10 13:52:14 -0700

    buttons

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
+function makeButton() {
+  const button = document.createElement('button')
+  button.innerText='eee'
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/cool', function(req, res, next) {
+  res.send('makeButton');
+});
+
 
 module.exports = router;
