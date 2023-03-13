26f021aef9eeb609276e506b88ade5ac0ae83ce4
jaugs <johnaugsburger@gmail.com>
2023-03-12 17:57:35 -0700

    set up mongoose, mongoDB and added dummy data

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+const mongoDB = process.env.URL_DB
 var app = express();
 
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+//
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
